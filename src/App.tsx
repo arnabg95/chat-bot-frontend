503b49d052a53ca091485da0d7d839e86520a1a7
Arnab gupta <arnab.gupta@weavers-web.com>
2024-04-17 13:45:29 +0530

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,64 +1,87 @@
-import { useState } from "react";
-import { Container, Row, Col, Form, Button } from "react-bootstrap";
+import React, { useState } from "react";
+import { Container, Row, Col, Form, Button, InputGroup } from "react-bootstrap";
+import Spinner from "react-bootstrap/Spinner";
 
 const App = () => {
+  const [loading, setLoading] = useState<boolean>(false);
   const [messages, setMessages] = useState<
     { text: string; type: string }[] | []
   >([]);
   const [inputText, setInputText] = useState("");
 
   const handleMessageSubmit = async (e: any) => {
-    e.preventDefault();
-    if (inputText.trim() === "") return;
-    setMessages((preVal: { text: string; type: string }[] | []) => {
-      return [...preVal, { text: inputText.trim(), type: "self" }];
-    });
-    setInputText("");
-    const res = await (
-      await fetch("http://127.0.0.1:8000/api/v1/chat/start-conversation", {
-        method: "POST",
-        body: JSON.stringify({ guest_id: "8968", chat_id: "5656", msg: inputText.trim() }),
-        headers: {
-          'Content-Type': 'application/json'
-        },
-      })
-    ).json();
-    setMessages((preVal: { text: string; type: string }[] | []) => {
-      return [...preVal, { text: res.message, type: "ai" }];
-    });
+    try {
+      e.preventDefault();
+      if (inputText.trim() === "") return;
+      setMessages((preVal: { text: string; type: string }[] | []) => {
+        return [...preVal, { text: inputText.trim(), type: "self" }];
+      });
+      setInputText("");
+      setLoading(true);
+
+      const res = await (
+        await fetch("http://127.0.0.1:8000/api/v1/chat/start-conversation", {
+          method: "POST",
+          body: JSON.stringify({
+            guest_id: "777",
+            chat_id: "888",
+            msg: inputText.trim(),
+          }),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        })
+      ).json();
+      setLoading(false);
+      setMessages((preVal: { text: string; type: string }[] | []) => {
+        return [...preVal, { text: res.message, type: "ai" }];
+      });
+    } catch (err) {
+      setLoading(false);
+    }
   };
 
   return (
-    <Container>
-      <Row>
+    <Container className="mycontainer">
+      <Row className="w-100">
         <Col>
           <div className="chat-window">
             {messages.map((message, index) => (
+              <React.Fragment key={index}>
               <div
-                key={index}
                 className={
                   message.type === "self" ? "user-message" : "bot-message"
                 }
-              >
+                style={{whiteSpace:"pre-wrap"}}
+                >
                 {message.text}
               </div>
+              <br />
+              </React.Fragment>
             ))}
           </div>
           <Form onSubmit={handleMessageSubmit} className="mt-3">
-            <Form.Group>
+            <InputGroup className="mb-3">
               <Form.Control
                 type="text"
                 placeholder="Type your message..."
                 value={inputText}
                 onChange={(e) => setInputText(e.target.value)}
               />
-            </Form.Group>
-            <Button variant="primary" type="submit">
-              Send
-            </Button>
+              <Button variant="primary" type="submit">
+                Send
+              </Button>
+            </InputGroup>
           </Form>
         </Col>
       </Row>
+      {loading ? (
+        <div className="loader">
+          <Spinner animation="border" role="status" variant="light">
+            <span className="visually-hidden">Loading...</span>
+          </Spinner>
+        </div>
+      ) : null}
     </Container>
   );
 };
